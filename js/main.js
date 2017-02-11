e0272c17528b8f8ec53f434fcf902589c3a81cfe
xiaoxu <932034728@qq.com>
2017-02-11 18:59:54 +0800

    my-resume

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     $('#fullPageSlide').FullPageSlide({
         navMenu: ['个人简介','技术栈','工作经历','项目经验','联系方式'],
-        author: '张旭 - 个人简历',
-        
+        author: '张旭 - 个人简历'
     });
+
+    // 初始化section3的结构
+    function renderskill () {
+
+    }
+
 });
